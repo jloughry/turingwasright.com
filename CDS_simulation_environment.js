049fc696f7ee6c47631c30c73981878f7fed8ca4
Joe Loughry <joe.loughry@gmail.com>
2018-01-01 18:51:25 -0700

    gap between canvases

--- CDS_simulation_environment.js
+++ CDS_simulation_environment.js
@@ -1,20 +1,29 @@
+border_thickness = 8; // border around canvases
+inter_canvas_gap = 6; // space between canvases
+
 function size_and_position(canvas_name) {
     var canvas = document.getElementById(canvas_name);
     var context = canvas.getContext("2d");
+
     var center_x = window.innerWidth / 2;
     var center_y = window.innerHeight / 2;
+    var center_x_with_gap = center_x + inter_canvas_gap / 2;
+    var center_y_with_gap = center_y + inter_canvas_gap / 2;
 
     // Size the canvas to fit.
-    canvas.width = center_x;
-    canvas.height = center_y;
+    canvas.width = center_x - (inter_canvas_gap / 2);
+    canvas.height = center_y - (inter_canvas_gap / 2);
 
     // Position the canvas in the window.
     center_x += "px";
     center_y += "px";
+    center_x_with_gap += "px";
+    center_y_with_gap += "px";
 
-    // Not defined inside the switch block because needed in this scope.
+    // These are not defined inside switch block because needed in this scope.
     var position_left; // undefined
     var position_top; // undefined
+    var adjustment = 2 * (border_thickness - 1);
 
     switch(canvas_name) {
         case "high_side_canvas":
@@ -27,32 +36,55 @@ function size_and_position(canvas_name) {
         case "high_side_classification_banner_canvas":
             position_left = "0px";
             position_top = "0px";
-            canvas.style.zIndex = 99;
+            canvas.width -= adjustment;
+            canvas.height -= adjustment;
+            canvas.style.zIndex = 99; // always on top
+            classify("high_side_classification_banner_canvas", "TS");
             break;
         case "low_side_canvas":
-            position_left = center_x;
-            position_top = center_y;
+            position_left = center_x_with_gap;
+            position_top = center_y_with_gap;
+            context.fillStyle = "lightgrey";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            canvas.style.zIndex = 1;
             break;
         case "low_side_classification_banner_canvas":
-            position_left = center_x;
-            position_top = center_y;
-            canvas.style.zIndex = 99;
+            position_left = center_x_with_gap;
+            position_top = center_y_with_gap;
+            canvas.width -= adjustment;
+            canvas.height -= adjustment;
+            canvas.style.zIndex = 99; // always on top
+            classify("low_side_classification_banner_canvas", "unclas");
             break;
         case "monitoring_canvas":
             position_left = "0px";
-            position_top = center_y;
+            position_top = center_y_with_gap;
+            context.fillStyle = "navy";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            canvas.style.zIndex = 1;
             break;
         case "monitoring_classification_banner_canvas":
             position_left = "0px";
-            position_top = center_y;
+            position_top = center_y_with_gap;
+            canvas.width -= adjustment;
+            canvas.height -= adjustment;
+            canvas.style.zIndex = 99; // always on top
+            classify("monitoring_classification_banner_canvas", "SYSTEM HIGH");
             break;
         case "control_canvas":
-            position_left = center_x;
+            position_left = center_x_with_gap;
             position_top = "0px"
+            context.fillStyle = "darkgreen";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            canvas.style.zIndex = 1;
             break;
         case "control_classification_banner_canvas":
-            position_left = center_x;
+            position_left = center_x_with_gap;
             position_top = "0px"
+            canvas.width -= adjustment;
+            canvas.height -= adjustment;
+            canvas.style.zIndex = 99; // always on top
+            classify("control_classification_banner_canvas", "SYSTEM HIGH");
             break;
         default:
             alert("incipient wombat yellow");
@@ -76,34 +108,36 @@ function label(canvas_name, label) {
 function classify(canvas_name, security_label) {
     var canvas = document.getElementById(canvas_name);
     var context = canvas.getContext("2d");
-    var banner_height = 17;
+    var label_size = 15;
+    var banner_height = label_size;
+
+    context.font = "bold " + label_size + "px sans-serif";
+    context.textBaseline = "hanging";
+    context.textAlign = "center";
 
     switch(security_label) {
-        case "UNCLASSIFIED":
-            context.fillStyle = "lime";
+        case "unclas":
+            color = "lime";
+            canvas.style.border = border_thickness + "px solid " + color;
+            context.fillStyle = color;
             context.fillRect(0, 0, canvas.width, banner_height);
             context.fillStyle = "black";
-            context.font = "bold 15px sans-serif";
-            context.textBaseline = "top";
-            context.textAlign = "center";
             context.fillText("UNCLASSIFIED", canvas.width / 2, 0);
             break;
-        case "TOP SECRET":
-            context.fillStyle = "orange";
+        case "TS":
+            color = "orange";
+            canvas.style.border = border_thickness + "px solid " + color;
+            context.fillStyle = color;
             context.fillRect(0, 0, canvas.width, banner_height);
             context.fillStyle = "black";
-            context.font = "bold 15px sans-serif";
-            context.textBaseline = "top";
-            context.textAlign = "center";
-            context.fillText("TOP SECRET", canvas.width / 2, 0);
+            context.fillText("TOP SECRET//SI TK", canvas.width / 2, 0);
             break;
         case "SYSTEM HIGH":
-            context.fillStyle = "red";
+            color = "red";
+            canvas.style.border = border_thickness + "px solid " + color;
+            context.fillStyle = color;
             context.fillRect(0, 0, canvas.width, banner_height);
             context.fillStyle = "white";
-            context.font = "bold 15px sans-serif";
-            context.textBaseline = "top";
-            context.textAlign = "center";
             context.fillText("SYSTEM HIGH", canvas.width / 2, 0);
             break;
         default:
@@ -122,13 +156,10 @@ function label_all() {
 function initialise() {
     size_and_position("high_side_canvas");
     size_and_position("high_side_classification_banner_canvas");
-    classify("high_side_classification_banner_canvas", "TOP SECRET");
     size_and_position("low_side_canvas");
     size_and_position("low_side_classification_banner_canvas");
-    classify("low_side_classification_banner_canvas", "UNCLASSIFIED");
     size_and_position("monitoring_canvas");
     size_and_position("monitoring_classification_banner_canvas");
-    classify("monitoring_classification_banner_canvas", "SYSTEM HIGH");
     size_and_position("control_canvas");
     size_and_position("control_classification_banner_canvas");
     classify("control_classification_banner_canvas", "SYSTEM HIGH");
